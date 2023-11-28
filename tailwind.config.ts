d58664cc28cbbc186ce79b780ade1ff1157b92ca
gilangakbarnugroho <likeamanlyman@gmail.com>
2023-11-28 10:39:37 +0700

    first set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,17 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
+      colors: {
+        'idnft' : { DEFAULT: '#DE4942' , 50: '#FEEAF1' , 100: '#FFC2BF' , 200: '#FF746D' , 400: '#DE4942' , 500: '#BE0C3C' , 700: '#A12E2E' , 900: '#990B31' },
+      },
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"' , 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,5 +25,4 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
