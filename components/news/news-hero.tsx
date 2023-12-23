39e78bf78c24e7412c87da55597fb663425da887
gilangakbarnugroho <likeamanlyman@gmail.com>
2023-12-23 23:33:16 +0700

    update components

--- components/news/news-hero.tsx
+++ components/news/news-hero.tsx
@@ -22,9 +22,9 @@ export default function NewsHero() {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-rose-700 to-red-700">Community Center</span></h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-rose-700 to-red-700">IDNFT News</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-lg text-gray-600 mb-4" data-aos="zoom-y-out" data-aos-delay="150">Join our community group channel and follow IDNFT social media for the latest updates on the Web3 and NFT industry in Indonesia! Stay informed and be part of the evolving digital landscape.</p>
+              <p className="text-lg text-gray-600 mb-4" data-aos="zoom-y-out" data-aos-delay="150">Lorem ipsum dolor sit amet</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
               </div>
             </div>
