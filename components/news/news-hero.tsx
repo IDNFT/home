1877cef1f62469c4d1e9f0940f13ee2693803411
gilangakbarnugroho <likeamanlyman@gmail.com>
2023-12-24 19:28:07 +0700

    update components

--- components/news/news-hero.tsx
+++ components/news/news-hero.tsx
@@ -24,7 +24,7 @@ export default function NewsHero() {
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-rose-700 to-red-700">IDNFT News</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-lg text-gray-600 mb-4" data-aos="zoom-y-out" data-aos-delay="150">Lorem ipsum dolor sit amet</p>
+              <p className="text-lg text-gray-600 mb-4" data-aos="zoom-y-out" data-aos-delay="150">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non urna eu neque porttitor pharetra nec quis libero.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
               </div>
             </div>
